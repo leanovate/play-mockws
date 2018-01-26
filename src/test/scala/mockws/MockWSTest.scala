leanovate/play-mockws

commit 26aec991f50a7226dc4df76495dd546a8045b712

Fix withHttpHeaders behavior for FakeWSRequestHolder
When calling this method old headers are discarded

src/main/scala/mockws/FakeWSRequestHolder.scala
@@ -66,7 +66,7 @@ case class FakeWSRequestHolder(
   def withHeaders(hdrs: (String, String)*): Self = withHttpHeaders(hdrs: _*)
 
   def withHttpHeaders(hdrs: (String, String)*): Self = {
-    val headers = hdrs.foldLeft(this.headers)(
+    val headers = hdrs.foldLeft(Map.empty[String, Seq[String]])(
       (m, hdr) =>
         if (m.contains(hdr._1)) m.updated(hdr._1, m(hdr._1) :+ hdr._2)
         else m + (hdr._1 -> Seq(hdr._2))

src/test/scala/mockws/MockWSTest.scala
@@ -282,4 +282,22 @@ class MockWSTest extends FunSuite with Matchers with PropertyChecks {
 
     ws.close()
   }
+
+  test("discard old headers when setting withHttpHeaders") {
+    var headers: Map[String, scala.Seq[String]] = Map.empty
+    val ws = MockWS {
+      case (GET, "/get") => Action {
+        req =>
+          headers = req.headers.toMap
+          Ok(req.headers.getAll("key1").zipWithIndex.toString) }
+    }
+    val request = ws.url("/get")
+      .withHttpHeaders("key1" -> "value1")
+      .withHttpHeaders("key2" -> "value2")
+      .get()
+
+    await(request)
+    headers.get("key1") shouldBe None
+    headers.get("key2") shouldBe Some(Seq("value2"))
+  }
 }

commit 13d5df3ad59df4294bbe55a54f2fdb585423c7d7

Use `addHttpHeaders` in `withBodyAndContentType`
This fixes a regression introduced in commit
26aec991f50a7226dc4df76495dd546a8045b712 after which `withHttpHeaders` discards
old headers.

src/main/scala/mockws/FakeWSRequestHolder.scala
@@ -221,7 +221,7 @@ case class FakeWSRequestHolder(
     if (headers.contains("Content-Type")) {
       withBody(wsBody)
     } else {
-      withBody(wsBody).withHttpHeaders("Content-Type" -> contentType)
+      withBody(wsBody).addHttpHeaders("Content-Type" -> contentType)
     }
   }
 }

src/test/scala/mockws/MockWSTest.scala
@@ -319,4 +319,21 @@ class MockWSTest extends FunSuite with Matchers with PropertyChecks {
     queryMap.get("bar") shouldBe Some(Seq("bah"))
   }
 
+  test("keep headers with content type from BodyWritable") {
+    val headers = new AtomicReference[Map[String, scala.Seq[String]]](Map.empty)
+    val ws = MockWS {
+      case (POST, "/") => Action {
+        req =>
+          headers.set(req.headers.toMap)
+          Ok }
+    }
+    val request = ws.url("/")
+      .withHttpHeaders("key1" -> "value1")
+      .post("test")
+
+    val response = await(request)
+    response.status shouldBe OK
+    val headersMap = headers.get()
+    headersMap.get("key1") shouldBe Some(Seq("value1"))
+  }
 }