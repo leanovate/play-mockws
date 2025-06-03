3d4421e88cfb74c275fc876ad3e4df077bb4fb97
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2025-06-03 19:05:42 +0000

    remove workflow ro run renovate, use GitHub app

--- .github/renovate-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    branchPrefix: 'renovate/',
-    username: 'Renovate Bot',
-    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    platform: 'github',
-    repositories: ['leanovate/play-mockws'],
-    packageRules: [],
-};
